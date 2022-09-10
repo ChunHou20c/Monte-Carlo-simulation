9e68b7367c9a4c59b5768ab04644e78548cb59d9
chunhou20c <youmustbekiddingmeshit@gmail.com>
2022-09-10 20:41:55 +0800

    Add processing of directory name

--- Frame.py
+++ Frame.py
@@ -6,14 +6,27 @@ class frame():
         """The constructor to read all the data from the text file, take path of the file as argument"""
 
         with open(path) as frame_data:
+
+        #do some processing first to cleanup unused line
+        #first line is the frame number
+
+            first_line = next(frame_data)
+            step_number = first_line.split()[-1]
+            self.dir_name = f'./{step_number}/' #this last element is the step we want  
+            #The step number will be used to  construct the folder name for the molecules and pairs
             
-            for lines in frame_data:
-                print(lines)
-                break
+            self.number_of_line = next(frame_data) #this is the number of line to be read as molecular data
+
+    def print_attributes(self):
+        """This method can be used to check the attributes of the data"""
+        
+        print(f'directory to save = {self.dir_name}')
+        print(f'number of lines to save = {self.number_of_line}')
 
 def main():
 
     frame0 = frame('Frames/DBT1-00')
+    frame0.print_attributes()
 
 if(__name__=='__main__'):
 
