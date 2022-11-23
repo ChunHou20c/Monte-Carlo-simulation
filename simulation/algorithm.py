9e3d381cf441c2afb4854e49266aa83b45170a78
chunhou20c <youmustbekiddingmeshit@gmail.com>
2022-11-23 13:25:27 +0800

    add algorithm for closest pair detection in periodic space

--- simulation/algorithm.py
+++ simulation/algorithm.py
@@ -1,5 +1,5 @@
 """This module contains the algorithm that is used by this project only"""
-from typing import Union
+from typing import Optional, Union
 from simulation import DBT1
 from simulation.molecule_relation import Relation
 from itertools import product
@@ -79,25 +79,69 @@ def DBT1_pair_is_close(m1:DBT1.DBT1, m2: DBT1.DBT1, cut_off:float, Stride, Trans
 
     return False
 
+def _translation_checker(x1:tuple[float, float, float], y1:tuple[float, float, float], z1:tuple[float, float, float],
+                                 x2:tuple[float, float, float], y2:tuple[float, float, float], z2:tuple[float, float, float],
+                                 stride:float)->tuple[int, int, int]:
+    """This function should return the most possible transformation for the nearest distance in the periodic space"""
+
+    #check the 9 vectors of x, then check the 9 vectors of y, then z
+    
+    def vec_checker(tup1:tuple[float, float, float], tup2:tuple[float, float, float]):
+        """
+        this function check if there is a possible transformation that can reduce the distance in the periodic space,
+        if there is no way to reduce anymore, then 0 will be return, 
+        if a negative transformation is possible then return -1, and vice versa
+        """
+        avg_dist = 0
+
+        for i in product(tup2, tup1):
+            
+            vect = i[0] - i[1]
+            if -0.5*stride <= vect <= 0.5*stride: #if any of the vector fall into this region the check can be stopped immediately, return no transformation
+            
+                return 0
+
+            avg_dist += vect/9
+        
+        if avg_dist > 0:
+
+            return -1
+
+        return 1
+    
+    x_transformation = vec_checker(x1, x2)
+    y_transformation = vec_checker(y1, y2)
+    z_transformation = vec_checker(z1, z2)
+
+    return x_transformation, y_transformation, z_transformation
+
+def periodic_translation(m1:DBT1.DBT1, m2:DBT1.DBT1, Stride):
+    """This function is a helper/wrapper function to pass arguments into the transformation function"""
+
+    Coord1, Coord2 = m1.N_S1_S2_coordinates(Stride), m2.N_S1_S2_coordinates(Stride)
+    return _translation_checker(*Coord1, *Coord2, Stride)
+
 def molecular_pair_relation(frm:DBT1.DBT1, to:DBT1.DBT1, cut_off:float, Stride:float)->Union[Relation, None]:
     """This function should tell the relationship between 2 molecules,
     if there is no relation then it should return None"""
 
-    if DBT1_pair_is_close(frm, to, cut_off, Stride):
+    translation = periodic_translation(frm, to, Stride)
+    #translation = (0, 0, 0)
+    if DBT1_pair_is_close(frm, to, cut_off, Stride, translation):
 
-        return Relation(frm, to, (0, 0, 0))
-
+        return Relation(frm, to, translation)
+    
     return None
 
     #this part will check the transformation
-    tup = (-1, 0, 1)
-    ls = [i for i in product(tup, tup, tup) if i not in [(0, 0, 0)]]
+    #tup = (-1, 0, 1)
+    #ls = [i for i in product(tup, tup, tup) if i not in [(0, 0, 0)]]
 
-    for i in ls:
+    #for i in ls:
 
-        if DBT1_pair_is_close(frm, to, cut_off, Stride, i):
+    #    if DBT1_pair_is_close(frm, to, cut_off, Stride, i):
 
-            print(f"pair found at translation {i}")
-            return Relation(frm, to, i)
+    #        print(f"pair found at translation {i}")
+    #        return Relation(frm, to, i)
 
-    return None
+    #return None
