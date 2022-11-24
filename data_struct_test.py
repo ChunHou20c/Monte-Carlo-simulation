a663ffcb4b8ce7ef19d69e690feb27ee12211394
chunhou20c <youmustbekiddingmeshit@gmail.com>
2022-11-24 23:01:20 +0800

    add algorithm test

--- data_struct_test.py
+++ data_struct_test.py
@@ -4,6 +4,7 @@ from simulation.DBT1 import DBT1
 from simulation import constructors
 from simulation import algorithm
 from itertools import combinations
+from simulation.molecule_graph import Molecule_graph as Graph
 
 def molecular_data_struct_test():
     atom1 = atom(1,1,1,12)
@@ -28,17 +29,26 @@ def cube_test():
     cube2 = cube1.neighbor_cube(1).neighbor_cube(relative_y=-1)
     print(cube2)
 
-file_data = constructors._Gro_file_parser('Frames/DBT1-00', 56)
-molecules = file_data['list_of_molecules']
-#do a completion on all the molecules
-for index,molecule in enumerate(molecules):
-    molecules[index] = algorithm.complete_molecule(molecule)
+def algorithm_test():
+    file_data = constructors._Gro_file_parser('Frames/test.gro', 56)
+    molecules = file_data['list_of_molecules']
+    boundary_data = file_data['boundary']
+    graph = Graph()
+    #do a completion on all the molecules
+    for index,molecule in enumerate(molecules):
+        molecules[index] = algorithm.complete_molecule(molecule, boundary_data)
 
-count = 0
-for m1, m2 in combinations(molecules, 2):
-    
-    if (algorithm.molecular_pair_relation(m1, m2, 1.2, 9.88696) is not None):
 
-        count = count + 1
+    count = 0
+    for m1, m2 in combinations(molecules, 2):
 
-print(f'number of pair found is {count}')
+        relation = algorithm.molecular_pair_relation(m1, m2, 1.2, boundary_data)
+        if (relation is not None):
+
+            graph.add_edge(m1,m2,relation)
+            count = count + 1
+
+    print(f'number of pair found is {count}')
+    print(graph.num_vertex)
+
+algorithm_test()
