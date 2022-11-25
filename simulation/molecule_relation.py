70cba019c753db5629a2cf4b76ac1214a45fb540
chunhou20c <youmustbekiddingmeshit@gmail.com>
2022-11-25 17:55:20 +0800

    change molecule relation to not include molecule

--- simulation/molecule_relation.py
+++ simulation/molecule_relation.py
@@ -6,15 +6,18 @@ from simulation import DBT1
 class Relation:
     """This class stores the relationship between 2 molecule"""
 
-    def __init__(self, m1:DBT1.DBT1, m2:DBT1.DBT1, translation:tuple[int, int, int] = (0, 0, 0)) -> None:
+    def __init__(self, translation:tuple[int, int, int] = (0, 0, 0)) -> None:
         
-        self.m1 = m1
-        self.m2 = m2
         self.translation = translation
 
     def conjugate(self)->Relation:
         """This method return the conjugate of the relation"""
 
         translation = tuple(-i for i in self.translation)
-        return Relation(self.m2, self.m1, translation)
-        
+        return Relation(translation)
+ 
+
+def create_relation(m1:DBT1.DBT1, m2:DBT1.DBT1, translation: tuple[int, int, int])->Relation:
+    """This function creates the relation between 2 molecules"""
+
+    return Relation(translation)
