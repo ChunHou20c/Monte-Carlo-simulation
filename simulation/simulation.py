fa7a92fda0f725f2c384adec131234ed8c5d0c1e
chunhou20c <youmustbekiddingmeshit@gmail.com>
2022-11-25 17:55:48 +0800

    add run simulation method in the simulation class

--- simulation/simulation.py
+++ simulation/simulation.py
@@ -6,9 +6,14 @@ including the stopping condition, model to use and cut off distance for molecula
 """
 
 from simulation import constructors, algorithm
+from simulation import DBT1
 from simulation.molecule_graph import Molecule_graph as Graph
+from simulation.molecule_graph import Molecule_vertex
 from itertools import combinations
 
+import random
+import numpy as np
+from simulation.molecule_relation import Relation
 
 class Simulation:
     """This class define how the simulation should be run from a single frame"""
@@ -17,7 +22,62 @@ class Simulation:
         """gro_file - the file path of the gromac file"""
         
         self.__file__ = gro_file
-        self.graph = build_graph(gro_file, 56)
+        self.graph, self.box_width = build_graph(gro_file, 56)
+        self.total_jump = 10000
+        self.initial_box = np.array([0,0,0])
+        self.current_box = np.array([0,0,0])
+
+        print(self.box_width)
+
+    def run(self):
+        """this method run the simulation for electron jumps in the periodic space"""
+        
+        #start by choosing a random molecule - vertex
+
+        initial_key = random.choice([ i for i in self.graph.get_vertices()])
+        
+        initial_vertex = self.graph.get_vertex(initial_key)
+        print(initial_vertex.id)
+
+        current_vertex = initial_vertex
+        
+        def jump(vertex:Molecule_vertex):
+            """This function perform a jump and return the next vertex"""
+
+            #choose a neigbour
+            neigbours = vertex.get_connections()
+            key = random.choice([i for i in neigbours])
+
+            relation:Relation = vertex.get_weight(key)
+
+            #this part is reserved for the calculation of the jumping time
+
+            #for now we only care about the box tracking
+
+            return key, np.array(relation.translation)
+        
+        for i in range(self.total_jump):
+
+            new_key, translation = jump(current_vertex)
+            
+            current_vertex = self.graph.get_vertex(new_key)
+
+            self.current_box += translation
+
+        print(f'final box = {self.current_box}')
+
+        print(type(initial_vertex))
+        print(type(current_vertex))
+        
+        initial_molecule = initial_vertex.molecule
+        final_molecule = current_vertex.molecule
+
+        Coord1 = initial_molecule.N_S1_S2_coordinates(self.box_width, tuple(self.initial_box))
+        Coord2 = final_molecule.N_S1_S2_coordinates(self.box_width, tuple(self.current_box))
+
+        distance = DBT1.DBT1_distance(*Coord1, *Coord2)
+
+        print(f'distance travelled = {distance}')
 
 
 
@@ -28,6 +88,7 @@ def build_graph(file_path:str, chunk_size:int)->Graph:
     molecules = file_data['list_of_molecules']
     boundary_data = file_data['boundary']
     graph = Graph()
+    print(type(graph))
 
 
     for index,molecule in enumerate(molecules):
@@ -40,7 +101,7 @@ def build_graph(file_path:str, chunk_size:int)->Graph:
 
             graph.add_edge(m1,m2,relation)
 
-    return graph
+    return graph, boundary_data
 
 
 
