1a66899f6f602ab5a304b8a01133ccc84098438c
chunhou20c <youmustbekiddingmeshit@gmail.com>
2022-11-30 13:45:24 +0800

    add marcus equation

---
+++ electron_coupling/marcus_equation.py
@@ -0,0 +1,26 @@
+"""This module define Marcus's equation of electron transfer"""
+from math import pi, exp
+
+def transfer_rate(electron_coupling:float, reorganization_energy:float, temperature:float, Eij:float = 0):
+    """
+    This function define the calculation of marcus transfer rate
+    all the value are in ev
+    temperature in Kelvin
+    """
+
+    reduced_plack_constant = 6.582119569e-16
+    boltzmann_constant = 8.617333262e-5
+    
+    term1 = 2*pi/reduced_plack_constant
+
+    term2 = electron_coupling**2
+
+    term3 = (1/(4*pi*boltzmann_constant*temperature*reorganization_energy))**0.5
+
+    inside_exponent = -((Eij + reorganization_energy)**2)/(4*boltzmann_constant*temperature*reorganization_energy)
+    
+    term4 = exp(inside_exponent)
+
+    return term1*term2*term3*term4
+
+print('{:e}'.format(transfer_rate(0.8, 0.18, 293)))
