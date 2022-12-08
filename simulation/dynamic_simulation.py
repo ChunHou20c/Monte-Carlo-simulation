90aaa51365388958676e50b32d76f003fef2b5e5
chunhou20c <youmustbekiddingmeshit@gmail.com>
2022-12-08 14:00:31 +0800

    add dynamic simulation run method

--- simulation/dynamic_simulation.py
+++ simulation/dynamic_simulation.py
@@ -6,13 +6,48 @@ through the simulation list
 """
 
 from simulation import simulation
+from simulation import algorithm
 import os
+import random
+import numpy as np
 
 class dynamic_simulation:
 
     def __init__(self, static_frames:list[simulation.Simulation]) -> None:
         
-        self.frames_dict = build_dict(static_frames)
+        self.frames_dict:dict[float, simulation.Simulation] = build_dict(static_frames)
+        self.timeframe = 0
+        self.total_jump = 100
+        self.coordinates = []
+        self.vector = np.array((0.0000,0.0000,0.0000))
+
+    def inspect(self):
+
+        print(self.frames_dict.keys())
+
+    def run(self):
+        """This is the main loop for the simulation"""
+
+        current_frame = self.frames_dict[self.timeframe]
+        keys = list(current_frame.graph.get_vertices())
+        current_key = random.choice(keys)
+        
+        initial_coordinate = np.array(current_frame.graph.get_vertex(current_key).molecule.center_coordinate(current_frame.box_width, (0,0,0)))
+        self.coordinates.append(initial_coordinate)
+
+        for _ in range(self.total_jump):
+            
+            next_frame = algorithm.closest_value(list(self.frames_dict.keys()), self.timeframe)
+            current_frame = self.frames_dict[next_frame]
+            
+            current_key, jumping_time, vector = current_frame.single_jump(current_key)
+            
+            new_coordinate = self.coordinates[-1] + np.array(vector)
+            
+            self.coordinates.append(new_coordinate)
+            self.timeframe += jumping_time*1e12
+            print(f'{self.timeframe = }')
+            self.vector += np.array(vector)
     
 def build_dict(static_frames:list[simulation.Simulation])->dict:
     
@@ -29,8 +64,8 @@ def build_dynamic_simulation(dir:str):
 
     sim_list = []
     for file in os.listdir(dir):
-
-        sim = simulation.Simulation(f'{dir}/{file}')
+        full_path = os.path.join(dir, file)
+        sim = simulation.Simulation(full_path)
         sim_list.append(sim)
 
     Dynamic_sim = dynamic_simulation(sim_list)
